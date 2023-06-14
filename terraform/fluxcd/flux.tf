11d7f1081abaa1c445b704aab33925f0985d4af4
Kris Barkar <kristin.barkardottir@civicplus.com>
2023-06-14 11:45:21 +0200

    Finalize fluxcd (#8)
    Add fluxcd setup
    Setup csi driver
    Configure fluxcd for clusters and app tree setup
    
    Signed-off-by: Kris Barkar <kristin.barkardottir@civicplus.com>

--- terraform/fluxcd/flux.tf
+++ terraform/fluxcd/flux.tf
@@ -1,11 +1,29 @@
-provider "flux" {
-  kubernetes = {
-    config_path = "~/.kube/config"
-  }
-  git = {
-    url = var.repository_url
-  }
+provider "kubectl" {
+  host                   = module.gke_auth.host
+  cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
+  token                  = module.gke_auth.token
+  load_config_file       = false
 }
-resource "flux_bootstrap_git" "this" {
-  path = "fluxcd/clusters/sandbox"
+
+
+data "kubectl_path_documents" "install_fluxcd" {
+  pattern = "../../fluxcd/clusters/sandbox/flux-system/*.yaml"
 }
+
+resource "kubectl_manifest" "fluxcd" {
+  count     = length(data.kubectl_path_documents.install_fluxcd.documents)
+  yaml_body = element(data.kubectl_path_documents.install_fluxcd.documents, count.index)
+  override_namespace = "flux-system"
+}
+
+data "kubectl_path_documents" "install_apps" {
+  pattern = "../../fluxcd/clusters/sandbox/sandbox/*.yaml"
+}
+
+resource "kubectl_manifest" "apps" {
+  count     = length(data.kubectl_path_documents.install_fluxcd.documents)
+  yaml_body = element(data.kubectl_path_documents.install_fluxcd.documents, count.index)
+  override_namespace = "flux-system"
+  depends_on = [kubectl_manifest.fluxcd]
+}
+
--- terraform/fluxcd/providers.tf
+++ terraform/fluxcd/providers.tf
@@ -1,12 +1,26 @@
 terraform {
+  required_version = ">= 0.14"
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "4.27.0"
+      source  = "hashicorp/google"
+      version = ">= 4.27.0"
     }
-    flux = {
-      source = "fluxcd/flux"
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+
+      version = ">= 1.14.0"
+
     }
+    kustomization = {
+      source  = "kbst/kustomization"
+      version = ">= 0.9.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+
   }
-  required_version = ">= 0.14"
 }
+
+provider "kustomization" {}

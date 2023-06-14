e586507ed75be9cf4a55e43caa9924c7b2d9caca
Kris Barkar <kristin.barkardottir@civicplus.com>
2023-06-14 14:07:32 +0200

    Tfkube (#10)
    * fix configuration errors
    
    Signed-off-by: Kris Barkar <kristin.barkardottir@civicplus.com>
    
    * fix kubectl module patterns
    
    Signed-off-by: Kris Barkar <kristin.barkardottir@civicplus.com>
    
    ---------
    
    Signed-off-by: Kris Barkar <kristin.barkardottir@civicplus.com>

--- terraform/fluxcd/flux.tf
+++ terraform/fluxcd/flux.tf
@@ -16,14 +16,24 @@ resource "kubectl_manifest" "fluxcd" {
   override_namespace = "flux-system"
 }
 
-data "kubectl_path_documents" "install_apps" {
-  pattern = "../../fluxcd/clusters/sandbox/sandbox/*.yaml"
+data "kubectl_file_documents" "install_common" {
+  content = file("../../fluxcd/clusters/sandbox/sandbox/infrastructure.yaml")
 }
 
-resource "kubectl_manifest" "apps" {
-  count     = length(data.kubectl_path_documents.install_fluxcd.documents)
-  yaml_body = element(data.kubectl_path_documents.install_fluxcd.documents, count.index)
+resource "kubectl_manifest" "common" {
+  count     = length(data.kubectl_file_documents.install_common.documents)
+  yaml_body = element(data.kubectl_file_documents.install_common.documents, count.index)
   override_namespace = "flux-system"
   depends_on = [kubectl_manifest.fluxcd]
 }
 
+data "kubectl_file_documents" "install_infrastrucure" {
+  content = file("../../fluxcd/clusters/sandbox/sandbox/common.yaml")
+}
+
+resource "kubectl_manifest" "infrastructure" {
+  count     = length(data.kubectl_file_documents.install_infrastrucure.documents)
+  yaml_body = element(data.kubectl_file_documents.install_infrastrucure, count.index)
+  override_namespace = "flux-system"
+  depends_on = [kubectl_manifest.fluxcd]
+}
